206a3319356f5389a55a8e44ea5539812aba3761
Konstantin Belin <kbel01@mail.ru>
2023-02-10 15:20:41 +0700

    English localization

--- java-automation-epam-course-master/src/test/java/training/stage2/framework/service/TestingSetReader.java
+++ java-automation-epam-course-master/src/test/java/training/stage2/framework/service/TestingSetReader.java
@@ -1,9 +1,10 @@
 package training.stage2.framework.service;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class TestingSetReader {
-    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"), Locale.ENGLISH);
 
     public static String getTestingSet(String key) {
         return resourceBundle.getString(key);
